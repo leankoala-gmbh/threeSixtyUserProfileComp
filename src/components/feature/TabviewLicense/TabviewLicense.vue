4c66cb5b29d4bb85eefc9fbd2aab716d341bc0c4
saschafuchs <sascha.fuchs@webpros.com>
2023-01-20 19:10:20 +0100

    test: adding console log for testing request

--- src/components/feature/TabviewLicense/TabviewLicense.vue
+++ src/components/feature/TabviewLicense/TabviewLicense.vue
@@ -1,4 +1,12 @@
 <script lang="ts" setup>
+console.log('hello world')
+const getLicense = async () => {
+  const response = await fetch('https://monitoring.my-hosting.world/license/')
+  const data = response
+  console.log(data)
+}
+
+getLicense()
 </script>
 
 <template>
