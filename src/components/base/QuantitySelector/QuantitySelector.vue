38d54710d3bf1e22ac8896cc5d5ae147de9baeec
Jorge Flores <fjorge8@gmail.com>
2023-02-09 09:58:26 +0100

    feature: QuantitySelector

---
+++ src/components/base/QuantitySelector/QuantitySelector.vue
@@ -0,0 +1,85 @@
+<script lang="ts" setup>
+const props = defineProps({
+  min: {
+    type: Number,
+    default: 1
+  },
+  max: {
+    type: Number,
+    default: 10
+  },
+  value: {
+    type: Number,
+    default: 1
+  }
+})
+
+const emit= defineEmits(['changeQuantity'])
+
+const quantity = ref(props.value)
+
+type TOperations = 'minus'| 'plus'
+const operations = {
+  minus: ()=>{
+    if (isLowerLimit.value) return
+    quantity.value--
+  },
+  plus: ()=>{
+    if (isUpperLimit.value) return
+    quantity.value++
+  }
+}
+
+const handleQuantity = (operation: TOperations) => {
+  if (!isWithinRange.value) return
+  operations[operation]()
+  emit('changeQuantity', quantity)
+}
+const isWithinRange = computed(()=>{
+  return quantity.value >= props.min && quantity.value <= props.max
+})
+
+const isLowerLimit = computed(()=>{
+  return quantity.value === props.min
+})
+const isUpperLimit = computed(()=>{
+  return quantity.value === props.max
+})
+watch(() => quantity.value, () => {
+  if (isWithinRange || !isLowerLimit || !isUpperLimit) return
+}, { immediate: true })
+</script>
+
+<template>
+  <div class="quantitySelector flex flex-wrap mt-1">
+    <GeneralButton
+      :is-disabled="isLowerLimit"
+      variant="ghost"
+      @click="handleQuantity('minus')"
+    >
+      -
+    </GeneralButton>
+    <input
+      v-model="quantity"
+      aria-label="InputQuantity"
+      class="mx-2 rounded text-center"
+    >
+    <GeneralButton
+      :is-disabled="isUpperLimit"
+      variant="ghost"
+      @click="handleQuantity('plus')"
+    >
+      +
+    </GeneralButton>
+    <div class="errorMessage w-full">
+      <p v-if="!isWithinRange" class="text-red-500 text-xs">
+        Quantity must be between {{ props.min }} and {{ props.max }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+  .quantitySelector {}
+</style>
+
