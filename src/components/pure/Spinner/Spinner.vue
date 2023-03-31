a702c70bd32a2a56a47701dddd5beaf73255a875
saschafuchs <sascha.fuchs@webpros.com>
2023-03-31 09:33:29 +0200

    feature: add a loading spinner

---
+++ src/components/pure/Spinner/Spinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  ><circle
+    class="spinner"
+    cx="4"
+    cy="12"
+    r="3"
+  /><circle
+    class="spinner spinnerDot1"
+    cx="12"
+    cy="12"
+    r="3"
+  /><circle
+    class="spinner spinnerDot2"
+    cx="20"
+    cy="12"
+    r="3"
+  /></svg>
+</template>
